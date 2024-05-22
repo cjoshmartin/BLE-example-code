c1a4632c933d3bc2ca9f0e72e5d750e2c79aa8a7
Josh Martin <joshmartin@reprogrammed.software>
2024-05-22 15:59:55 -0500

    Sending BLE data

--- ardunio_code/ble_code/ble_code.ino
+++ ardunio_code/ble_code/ble_code.ino
@@ -2,6 +2,8 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 #include <BLE2902.h>
+#include <string>
+#include <iostream>
 
 BLEServer *pServer;
 #define DEVICE_NAME "MyESP32"
@@ -9,6 +11,27 @@ BLEServer *pServer;
 
 BLECharacteristic *pStepCountCharacteristic;
 #define STEPCOUNT_CHARACTERISTIC_UUID "fbb6411e-26a7-44fb-b7a3-a343e2b011fe"
+
+int heartRate = 0;
+BLECharacteristic *pHeartRateCharacteristic;
+# define HEARTRATE_CHARACTERISTIC_UUID "c58b67c8-f685-40d2-af4c-84bcdaf3b22e"
+
+int readValue(BLECharacteristic *characteristic) {
+  if (characteristic == NULL) {
+    return -1;
+  }
+  std::string value = std::string(
+      (
+        characteristic->getValue()
+      ).c_str()
+    );
+
+  try{
+    return stoi(value);
+  } catch(...) {
+    return -1;
+  }
+}
 
 class MyCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
@@ -43,6 +66,7 @@ void setup() {
   BLEService *pService = pServer->createService(SERVICE_UUID);
 
   pStepCountCharacteristic = createCharacteristic(STEPCOUNT_CHARACTERISTIC_UUID, pService);
+  pHeartRateCharacteristic = createCharacteristic(HEARTRATE_CHARACTERISTIC_UUID, pService);
 
     //start the service
   pService->start();
@@ -66,5 +90,16 @@ void loop() {
 
   Serial.print("Value is: ");
   Serial.println(stepCount);
+
+  heartRate = readValue(pHeartRateCharacteristic);
+  Serial.print("heart rate value is: ");
+  if (heartRate > 0){
+    Serial.println(heartRate);
+  }
+  else {
+    Serial.println("No Heart rate value received");
+  }
+
+
   delay(500);
 }
